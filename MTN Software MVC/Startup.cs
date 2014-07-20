71ce63347bdfd4120522062915697bc5298768c6
Thomas Maloney <thomastnf@live.com>
2014-07-20 03:11:03 -0500

    Should be final published stage

---
+++ MTN Software MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MTN_Software_MVC.Startup))]
+namespace MTN_Software_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
