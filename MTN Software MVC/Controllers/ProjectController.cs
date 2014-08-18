d13a70129fbe219738d05917d742e81ed5b07dd2
OceanOSdev <ThomasTNF@live.com>
2014-08-17 19:17:40 -0500

    Removed search bar, fixed links, added project controller

---
+++ MTN Software MVC/Controllers/ProjectController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MTN_Software_MVC.Controllers
+{
+    public class ProjectController : Controller
+    {
+        // GET: Project
+        public ActionResult Project()
+        {
+            return View();
+        }
+    }
+}
